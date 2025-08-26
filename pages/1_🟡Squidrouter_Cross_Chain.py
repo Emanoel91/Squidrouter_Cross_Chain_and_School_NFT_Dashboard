47b27e48249301fe6d9329a11fb2fa618a56bf86
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-08-26 23:54:05 +0330

    Update 1_🟡Squidrouter_Cross_Chain.py

--- pages/1_🟡Squidrouter_Cross_Chain.py
+++ pages/1_🟡Squidrouter_Cross_Chain.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import snowflake.connector
-import plotly.graph_objects as go
 import plotly.express as px
 import plotly.graph_objects as go
 from cryptography.hazmat.primitives import serialization
