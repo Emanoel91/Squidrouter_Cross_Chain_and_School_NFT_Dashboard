26b04533dcc34f139bd80bb0ffc34df51992f964
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-08-26 21:57:15 +0330

    Update 1_🟡Squidrouter_Cross_Chain.py

--- pages/1_🟡Squidrouter_Cross_Chain.py
+++ pages/1_🟡Squidrouter_Cross_Chain.py
@@ -14,56 +14,24 @@ st.set_page_config(
     layout="wide"
 )
 
-# --- Title  -----------------------------------------------------------------------------------------------------
-st.title("🟡Squidrouter Cross Chain")
+# --- Title with Logo -----------------------------------------------------------------------------------------------------
 
-# --- attention ---------------------------------------------------------------------------------------------------------
-st.info("📊Tables initially display data for a default time range. Select a custom range to view results for your desired period.")
-st.info("⏳On-chain data retrieval may take a few moments. Please wait while the results load.")
 
-# --- Sidebar Footer Slightly Left-Aligned ---
-st.sidebar.markdown(
+# --- Builder Info ---------------------------------------------------------------------------------------------------------
+st.markdown(
     """
-    <style>
-    .sidebar-footer {
-        position: fixed;
-        bottom: 20px;
-        width: 250px;
-        font-size: 13px;
-        color: gray;
-        margin-left: 5px; # -- MOVE LEFT
-        text-align: left;  
-    }
-    .sidebar-footer img {
-        width: 16px;
-        height: 16px;
-        vertical-align: middle;
-        border-radius: 50%;
-        margin-right: 5px;
-    }
-    .sidebar-footer a {
-        color: gray;
-        text-decoration: none;
-    }
-    </style>
-
-    <div class="sidebar-footer">
-        <div>
-            <a href="https://x.com/axelar" target="_blank">
-                <img src="https://img.cryptorank.io/coins/axelar1663924228506.png" alt="Axelar Logo">
-                Powered by Axelar
-            </a>
-        </div>
-        <div style="margin-top: 5px;">
-            <a href="https://x.com/0xeman_raz" target="_blank">
-                <img src="https://pbs.twimg.com/profile_images/1841479747332608000/bindDGZQ_400x400.jpg" alt="Eman Raz">
-                Built by Eman Raz
-            </a>
+    <div style="margin-top: 20px; margin-bottom: 20px; font-size: 16px;">
+        <div style="display: flex; align-items: center; gap: 10px;">
+            <img src="https://pbs.twimg.com/profile_images/1841479747332608000/bindDGZQ_400x400.jpg" style="width:25px; height:25px; border-radius: 50%;">
+            <span>Built by: <a href="https://x.com/0xeman_raz" target="_blank">Eman Raz</a></span>
         </div>
     </div>
     """,
     unsafe_allow_html=True
 )
+
+st.info("📊Charts initially display data for a default time range. Select a custom range to view results for your desired period.")
+st.info("⏳On-chain data retrieval may take a few moments. Please wait while the results load.")
 
 # --- Snowflake Connection ----------------------------------------------------------------------------------------
 snowflake_secrets = st.secrets["snowflake"]
@@ -86,7 +54,7 @@ private_key_bytes = private_key.private_bytes(
     encryption_algorithm=serialization.NoEncryption()
 )
 
-conn = snowflake.connector.connect( 
+conn = snowflake.connector.connect(
     user=user,
     account=account,
     private_key=private_key_bytes,
@@ -96,13 +64,134 @@ conn = snowflake.connector.connect(
 )
 
 # --- Date Inputs ---------------------------------------------------------------------------------------------------
+timeframe = st.selectbox("Select Time Frame", ["month", "week", "day"])
+start_date = st.date_input("Start Date", value=pd.to_datetime("2023-01-01"))
+end_date = st.date_input("End Date", value=pd.to_datetime("2025-07-31"))
+# --- Query Function: Row1 --------------------------------------------------------------------------------------
+@st.cache_data
+def load_kpi_data(timeframe, start_date, end_date):
+    
+    start_str = start_date.strftime("%Y-%m-%d")
+    end_str = end_date.strftime("%Y-%m-%d")
+
+    query = f"""
+    WITH axelar_service AS (
+        -- Token Transfers
+        SELECT 
+            created_at, 
+            LOWER(data:send:original_source_chain) AS source_chain, 
+            LOWER(data:send:original_destination_chain) AS destination_chain,
+            recipient_address AS user, 
+            CASE 
+              WHEN IS_ARRAY(data:send:amount) THEN NULL
+              WHEN IS_OBJECT(data:send:amount) THEN NULL
+              WHEN TRY_TO_DOUBLE(data:send:amount::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:send:amount::STRING)
+              ELSE NULL
+            END AS amount,
+            CASE 
+              WHEN IS_ARRAY(data:send:amount) OR IS_ARRAY(data:link:price) THEN NULL
+              WHEN IS_OBJECT(data:send:amount) OR IS_OBJECT(data:link:price) THEN NULL
+              WHEN TRY_TO_DOUBLE(data:send:amount::STRING) IS NOT NULL AND TRY_TO_DOUBLE(data:link:price::STRING) IS NOT NULL 
+                THEN TRY_TO_DOUBLE(data:send:amount::STRING) * TRY_TO_DOUBLE(data:link:price::STRING)
+              ELSE NULL
+            END AS amount_usd,
+            CASE 
+              WHEN IS_ARRAY(data:send:fee_value) THEN NULL
+              WHEN IS_OBJECT(data:send:fee_value) THEN NULL
+              WHEN TRY_TO_DOUBLE(data:send:fee_value::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:send:fee_value::STRING)
+              ELSE NULL
+            END AS fee,
+            id, 
+            'Token Transfers' AS Service, 
+            data:link:asset::STRING AS raw_asset
+        FROM axelar.axelscan.fact_transfers
+        WHERE status = 'executed'
+          AND simplified_status = 'received'
+          AND (
+            sender_address ilike '%0xce16F69375520ab01377ce7B88f5BA8C48F8D666%' 
+            OR sender_address ilike '%0x492751eC3c57141deb205eC2da8bFcb410738630%'
+            OR sender_address ilike '%0xDC3D8e1Abe590BCa428a8a2FC4CfDbD1AcF57Bd9%'
+            OR sender_address ilike '%0xdf4fFDa22270c12d0b5b3788F1669D709476111E%'
+            OR sender_address ilike '%0xe6B3949F9bBF168f4E3EFc82bc8FD849868CC6d8%'
+          )
+
+        UNION ALL
+
+        -- GMP
+        SELECT  
+            created_at,
+            data:call.chain::STRING AS source_chain,
+            data:call.returnValues.destinationChain::STRING AS destination_chain,
+            data:call.transaction.from::STRING AS user,
+            CASE 
+              WHEN IS_ARRAY(data:amount) OR IS_OBJECT(data:amount) THEN NULL
+              WHEN TRY_TO_DOUBLE(data:amount::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:amount::STRING)
+              ELSE NULL
+            END AS amount,
+            CASE 
+              WHEN IS_ARRAY(data:value) OR IS_OBJECT(data:value) THEN NULL
+              WHEN TRY_TO_DOUBLE(data:value::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:value::STRING)
+              ELSE NULL
+            END AS amount_usd,
+            COALESCE(
+              CASE 
+                WHEN IS_ARRAY(data:gas:gas_used_amount) OR IS_OBJECT(data:gas:gas_used_amount) 
+                  OR IS_ARRAY(data:gas_price_rate:source_token.token_price.usd) OR IS_OBJECT(data:gas_price_rate:source_token.token_price.usd) 
+                THEN NULL
+                WHEN TRY_TO_DOUBLE(data:gas:gas_used_amount::STRING) IS NOT NULL 
+                  AND TRY_TO_DOUBLE(data:gas_price_rate:source_token.token_price.usd::STRING) IS NOT NULL 
+                THEN TRY_TO_DOUBLE(data:gas:gas_used_amount::STRING) * TRY_TO_DOUBLE(data:gas_price_rate:source_token.token_price.usd::STRING)
+                ELSE NULL
+              END,
+              CASE 
+                WHEN IS_ARRAY(data:fees:express_fee_usd) OR IS_OBJECT(data:fees:express_fee_usd) THEN NULL
+                WHEN TRY_TO_DOUBLE(data:fees:express_fee_usd::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:fees:express_fee_usd::STRING)
+                ELSE NULL
+              END
+            ) AS fee,
+            id, 
+            'GMP' AS Service, 
+            data:symbol::STRING AS raw_asset
+        FROM axelar.axelscan.fact_gmp 
+        WHERE status = 'executed'
+          AND simplified_status = 'received'
+          AND (
+            data:approved:returnValues:contractAddress ilike '%0xce16F69375520ab01377ce7B88f5BA8C48F8D666%' 
+            OR data:approved:returnValues:contractAddress ilike '%0x492751eC3c57141deb205eC2da8bFcb410738630%'
+            OR data:approved:returnValues:contractAddress ilike '%0xDC3D8e1Abe590BCa428a8a2FC4CfDbD1AcF57Bd9%'
+            OR data:approved:returnValues:contractAddress ilike '%0xdf4fFDa22270c12d0b5b3788F1669D709476111E%'
+            OR data:approved:returnValues:contractAddress ilike '%0xe6B3949F9bBF168f4E3EFc82bc8FD849868CC6d8%'
+          )
+    )
+    SELECT 
+        COUNT(DISTINCT id) AS Number_of_Transfers, 
+        COUNT(DISTINCT user) AS Number_of_Users, 
+        ROUND(SUM(amount_usd)) AS Volume_of_Transfers
+    FROM axelar_service
+    WHERE created_at::date >= '{start_str}' 
+      AND created_at::date <= '{end_str}'
+    """
+
+    df = pd.read_sql(query, conn)
+    return df
+
+# --- Load Data ----------------------------------------------------------------------------------------------------
+df_kpi = load_kpi_data(timeframe, start_date, end_date)
+
+# --- KPI Row ------------------------------------------------------------------------------------------------------
 col1, col2, col3 = st.columns(3)
 
-with col1:
-    timeframe = st.selectbox("Select Time Frame", ["month", "week", "day"])
+col1.metric(
+    label="Volume of Transfers",
+    value=f"${df_kpi['VOLUME_OF_TRANSFERS'][0]:,}"
+)
 
-with col2:
-    start_date = st.date_input("Start Date", value=pd.to_datetime("2024-01-01"))
+col2.metric(
+    label="Number of Transfers",
+    value=f"{df_kpi['NUMBER_OF_TRANSFERS'][0]:,} Txns"
+)
 
-with col3:
-    end_date = st.date_input("End Date", value=pd.to_datetime("2025-08-31"))
+col3.metric(
+    label="Number of Users",
+    value=f"{df_kpi['NUMBER_OF_USERS'][0]:,} Addresses"
+)
