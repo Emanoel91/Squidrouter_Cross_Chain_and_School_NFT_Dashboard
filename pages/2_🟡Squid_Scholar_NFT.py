5449e9709ecb329a64ed47dfdd9a5746c18528be
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-08-27 21:35:22 +0330

    Update 2_🟡Squid_Scholar_NFT.py

--- pages/2_🟡Squid_Scholar_NFT.py
+++ pages/2_🟡Squid_Scholar_NFT.py
@@ -94,15 +94,3 @@ conn = snowflake.connector.connect(
     database=database,
     schema=schema
 )
-
-# --- Date Inputs ---------------------------------------------------------------------------------------------------
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    timeframe = st.selectbox("Select Time Frame", ["month", "week", "day"])
-
-with col2:
-    start_date = st.date_input("Start Date", value=pd.to_datetime("2024-01-01"))
-
-with col3:
-    end_date = st.date_input("End Date", value=pd.to_datetime("2025-08-31"))
